8af76f1a90506a02a30f457388e767adce5c46b8
Verita' Michele <verita.michele@osdbsesto>
2021-01-28 13:41:18 +0100

    get requests

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 var http = require("http");
 var express = require("express");
+var apiServer = express();
 
 var hostname = "127.0.0.1";
 var port = 3000;
+var nome = "NODEVERITA";
 
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Ciao belloccio!");
+
+apiServer.listen(port, hostname, () => {
+    console.log("server running at http://%s:/", hostname, port);
 });
 
-server.listen(port, hostname, () => {
-    console.log("server running at http://%s:%d/", hostname, port);
+apiServer.get("/nome", (req, res) =>{
+    console.log("ricevuto get su mioNome", req.query);
+    res.send("Il nome del server è: " + nome);
+});
+
+apiServer.get("/somma", (req, res) =>{
+    console.log();
+});
+
+apiServer.get("/nomeServer",(req, res) =>{
+    console.log("ricevuto get su nome", req);
+    res.send("Il nome del server è: " + nome);
+});
+
+apiServer.get("/", (req, res) =>{
+    res.send("Sei in home");
+});
+apiServer.get("/index",(req, res) =>{
+    res.sendFile("C:\\Users\\verita.michele\\Desktop\\juve merda\\provaNode\\provaNode\\index.html");
 });
