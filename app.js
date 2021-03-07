47ca11c5653f25eef01643fc34ba4773b012249f
MicheleTheTruth <verita.michele@studenti.salesianisesto.it>
2021-03-07 15:06:53 +0100

    fixed

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 var http = require("http");
+
 var express = require("express");
 var apiServer = express();
 
 var port = 3000;
-var nome = "NODEVERITA";
-
 
 apiServer.listen(port, () => {
-    console.log("server running on port", port);
+    console.log("server running on port ", port);
 });
 
 apiServer.get("/nome", (req, res) =>{
-    console.log("ricevuto get su mioNome", req.query);
-    res.send("Il nome del server è: " + nome);
+    console.log("ho ricevuto una get su nome");
+    res.send("ciao, il nome del server è: NODEPOGLIANI");
 });
 
-apiServer.get("/somma", (req, res) =>{
-    console.log();
-});
-
-apiServer.get("/nomeServer",(req, res) =>{
-    console.log("ricevuto get su nome", req);
-    res.send("Il nome del server è: " + nome);
-});
-
-apiServer.get("/", (req, res) =>{
-    res.send("Sei in home");
-});
-apiServer.get("/index",(req, res) =>{
-    res.sendFile("C:\\Users\\verita.michele\\Desktop\\juve merda\\provaNode\\provaNode\\index.html");
-});
+apiServer.get("/", function(req, res){
+    res.send("sei in home");
+})
